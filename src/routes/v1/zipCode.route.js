2b52a44c099505400ffe12b089838fa6884f8f01
haruelrovix <surat@havit.web.id>
2022-10-31 14:17:06 +0700

    docs: Add swagger documentation

--- src/routes/v1/zipCode.route.js
+++ src/routes/v1/zipCode.route.js
@@ -6,3 +6,38 @@ const router = express.Router();
 router.route('/:zipCode').get(zipCodeController.getZipCodeDetails);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: ZipCode
+ *   description: Zip Code retrieval
+ */
+
+/**
+ * @swagger
+ * /zip-code/{zipCode}:
+ *   get:
+ *     summary: Decode a zip code
+ *     description: Decode a zip code to get details about Province, City/Regency, District and Sub-District.
+ *     tags: [ZipCode]
+ *     parameters:
+ *       - in: path
+ *         name: zipCode
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: a 5 digits zip code
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/KeyValuePair'
+ */
